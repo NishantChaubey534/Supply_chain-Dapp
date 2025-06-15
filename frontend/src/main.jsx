83728a7e2526865b65e532022783852a96ff7932
Nishant Kumar Chaubey <chaubeynishant2@gmail.com>
2025-06-16 00:30:18 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css';
 import { Web3Provider } from './context/Web3Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
